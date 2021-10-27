c8863dcc24aaa7342adc392a777f84708d59dca9
Vova Krysa <ch.vova321@gmail.com>
2021-10-27 11:00:50 +0300

    Fix error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./styles/index.css";
 import { App } from "./components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
