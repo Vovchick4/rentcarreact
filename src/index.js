8270d44789a6113dab220dee550abf1a53c26bdf
Vova Krysa <ch.vova321@gmail.com>
2021-11-14 22:18:21 +0200

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import 'normalize.css';
 import './styles/variebles.css';
@@ -10,22 +12,27 @@ import './styles/toast.css';
 import './config/axios';
 import './config/i18n';
 
+import { store, persistor } from './redux/store';
 import { App } from './components';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-    <ToastContainer
-      position="bottom-center"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
